9e8a8c6085d6c55f38fcaff219179bcb0554f710
m.kuzneсova <m.kuzneova@peppers-studio.ru>
2024-01-16 17:51:40 +0300

    feat: добавлены setActive и getActive

--- action.js
+++ action.js
@@ -1,6 +1,15 @@
 class Action {
   constructor(data){
     this.data = data;
+    this.isActive = false;
+  }
+
+  setActive(state){
+    this.setActive = state;
+  }
+
+  getActive(){
+    return this.setActive;
   }
 
   toggleEnable(state){
