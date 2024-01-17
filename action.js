9930e05b496f16023c4c3e49bc8975e52762034f
m.kuzneсova <m.kuzneova@peppers-studio.ru>
2024-01-17 11:12:15 +0300

    feat: onChange

--- action.js
+++ action.js
@@ -4,14 +4,22 @@ class Action {
     this.data = data;
     this.isActive = isActive;
     this.enable = this.data.enable;
+    this.ACTION_EVENT = "action: event";
   }
 
   set activeState(state){
-    if (state !== this.isActive) this.isActive = state;
+    if (state === this.isActive) return; 
+    this.onChange();
+    this.isActive = state;
   }
 
   get activeState(){
+    this.addEventListener(this.ACTION_ACTIVATED, () => {}, false);
     return this.isActive;
+  }
+
+  onChange(){
+    this.dispatchEvent(new CustomEvent(this.ACTION_EVENT));
   }
 
   toggleEnable(state){
